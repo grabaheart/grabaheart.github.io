fc3b23e066d49c2384c23047ab3a1e6e544ca1eb
felek13 <patrykjon@gmail.com>
2018-02-28 11:35:24 +0100

    without description

--- _include/js/main.js
+++ _include/js/main.js
@@ -80,7 +80,7 @@ BRUSHED.slider = function(){
 											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">GBAH</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">GBAH</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">GBAH</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">GBAH</div>', thumb : '', url : ''}  
+											// {image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">GBAH</div>', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
